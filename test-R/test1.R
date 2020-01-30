efca4fa30c1e6f8d186df8cd60d87e92dd082477
SaoudiMehdi <58574228+SaoudiMehdi@users.noreply.github.com>
2020-01-30 20:25:34 +0100

    Test for-rnorm-ifelse

---
+++ test-R/test1.R
@@ -0,0 +1,14 @@
+for (i in 1:10) print(i)
+	y=z=0;
+for (i in 1:10) {
+	x=runif(1)
+	if (x>0.5) y=y+1
+	else z=z+1 }
+	y;z
+	for (i in c(2,4,5,8)) print(i)
+		x = rnorm(100)
+		y = ifelse(x>0, 1, -1) # condition
+		y;i=0
+		while (i<10){
+		print(i)
+		i=i+1}
