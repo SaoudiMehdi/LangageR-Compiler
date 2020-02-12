5c645862094f2df8c189ffc5245c7e092e9916e8
younessmouad <ymouad96@gmail.com>
2020-02-12 23:16:00 +0100

    push

---
+++ projet_CompilationV3/analyseur_syntaxique.h
@@ -0,0 +1,42 @@
+#ifndef ANALYSEUR_SYNTHAXIQUE_H
+#define ANALYSEUR_SYNTHAXIQUE_H
+
+
+#include "analyseur_lexical.h"
+#include "table_symboles.h"
+
+//declaration 
+
+
+
+
+
+void Test_Symbole(CODE_LEX code,CODE_ERR error);
+
+void INSTS();
+void INST();
+void AFFEC1();
+void AFFEC2();
+void AFFEC3();
+void SI();
+void TANTQUE();
+void ECRIRE();
+void LIRE();
+void COND();
+void EXPR();
+void TERM();
+void FACT();
+void POUR();
+void REPETER();
+void FOR();
+void MIN();
+void MAX();
+void PREMIER_SYM();
+void pvTest();
+void ECRIRE2();
+void GENERER1(MNEMONIQUES M);
+void GENERER2(MNEMONIQUES M, int val);
+void SaveInstToFile(INSTRUCTION INST, int i);
+void SavePCodeToFile();
+
+#endif
---
+++ projet_CompilationV3/table_symboles.h
@@ -0,0 +1,12 @@
+#ifndef TABLE_SYMBOLS_H
+#define TABLE_SYMBOLS_H
+
+#include "global.h"
+
+
+int CHERCHER_SYM(char* nom,OPTION option);
+int INDEX_SYM(char* nom);
+void AJOUTER_SYM(char* nom,TSYM type);
+
+
+#endif
